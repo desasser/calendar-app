9f7dcfc2d44c3b5122fdfdcc5e334f975f50691f
Daniel Sasser <desasser79@gmail.com>
2020-12-17 14:57:38 -0800

    entries save to local storage, need to update display when refreshed

--- script.js
+++ script.js
@@ -2,17 +2,20 @@
 var scheduleArr = JSON.parse(localStorage.getItem("schedInput")) || [];
 var scheduleObj = [];
 
+//Declare variables for local storage
+var thisHour = '';
+var newValue = '';
+
 //Upon loading page, grab anything in local storage and display it to the textarea corresponding to the timeOfDay value in the schedule object
 
 //Fetch current day of the week and date from moment.js
 var now = moment().format('dddd, MMMM Do YYYY');
+
 //Append current day to p-tag of currentDay in HTML
 $('#currentDay').append(now);
-// console.log(now);
 
 //Fetch current time from moment.js
 var currentTime = moment().format('HH')
-
 
 //Run loop to check which block current time fits in and assign class (past, future, present)
 for (i = 0; i < 24; i++) {
@@ -25,42 +28,22 @@ for (i = 0; i < 24; i++) {
             $(`[data-time = ${j}]`).prev().addClass('future');
         }
         //for all data-time < i, set the class attribute to future
-        for (let k = 0; k < i-1; k++) {
+        for (let k = 0; k < i; k++) {
             $(`[data-time = ${k}]`).prev().addClass('past');
         }
-    } 
-    
+    }
 }
-
-// console.log(currentTime);
 
 //When the save button is clicked, save the text from the present time into local storage
 $(".saveBtn").on("click", function() {
     
-    var dataTime = $(this).attr("data-time")
+    thisHour = $(this).attr("data-time");
+    newValue = $("#textEleven").val();
     
-    //Store calendar input from this item into object with the time and the content
-    //TODO: Use this object to maintain the text in the proper cells
-    scheduleObj = {
-        timeOfDay : dataTime,
-        schedInput : $(".present").val()
-    }
-    console.log(scheduleObj.timeOfDay);
-    
-    //Push schedule object into an array of objects
-    // scheduleArr.push(scheduleObj);
-    
-    //Set the data from users input into local storage for later use, stringified
-    // localStorage.setItem("schedInput", JSON.stringify($(".present").val()));
-    
-    //If the input event is blank but the button is clicked, return early doing nothing
-    if ($(".present").val() === '') {
-        return;
-    }
-    
+    localStorage.setItem(thisHour, newValue);
 })
 
 //Take the schedule from 4am and add it to the text area from 4 am
 //TODO: Come back to this, its not properly updating the screen
-// $("#textOne").text(scheduleObj.timeOfDay);
-// console.log(scheduleObj.timeOfDay);
+$("#textEleven").val(thisHour, newValue);
+console.log(thisHour, newValue);
